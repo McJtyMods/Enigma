1680b197b6340b6c476fdb2f83ebe45cbe8b209c
McJty <mcjty1@gmail.com>
2017-04-29 06:39:29 +0200

    Added a gold and silver coin

--- src/main/java/mcjty/enigma/items/ModItems.java
+++ src/main/java/mcjty/enigma/items/ModItems.java
@@ -6,13 +6,16 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class ModItems {
 
     public static Key key;
+    public static Coin coin;
 
     public static void init() {
         key = new Key();
+        coin = new Coin();
     }
 
     @SideOnly(Side.CLIENT)
     public static void initModels() {
         key.initModel();
+        coin.initModel();
     }
 }
