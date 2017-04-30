b1033ede473a5a499bb18a900a58e4bc687cc1a2
McJty <mcjty1@gmail.com>
2017-04-30 06:04:31 +0200

    Implemented isblock() function

--- src/main/java/mcjty/enigma/code/EnigmaExpressionContext.java
+++ src/main/java/mcjty/enigma/code/EnigmaExpressionContext.java
@@ -10,10 +10,12 @@ import mcjty.enigma.varia.BlockPosDim;
 import mcjty.enigma.varia.InventoryHelper;
 import mcjty.lib.tools.InventoryTools;
 import mcjty.lib.tools.ItemStackTools;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.common.DimensionManager;
 
 import javax.annotation.Nullable;
 import java.util.HashMap;
@@ -33,6 +35,15 @@ public class EnigmaExpressionContext implements ExpressionContext<EnigmaFunction
             Progress progress = ProgressHolder.getProgress(context.getWorld());
             PlayerProgress playerProgress = progress.getPlayerProgress(context.getPlayer().getPersistentID());
             return playerProgress.getState(o[0]);
+        });
+        FUNCTIONS.put("isblock", (context, o) -> {
+            Progress progress = ProgressHolder.getProgress(context.getWorld());
+            BlockPosDim namedPosition = progress.getNamedPosition(o[0]);
+            IBlockState state = progress.getNamedBlock(o[1]);
+            if (namedPosition != null && state != null) {
+                return DimensionManager.getWorld(namedPosition.getDimension()).getBlockState(namedPosition.getPos()).equals(state);
+            }
+            return false;
         });
         FUNCTIONS.put("hasitem", (context, o) -> {
             Progress progress = ProgressHolder.getProgress(context.getWorld());
